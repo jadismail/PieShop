ec8b368b6795be4c0ab50af174fb39dd912cee0b
Jad Ismail <jadismail665@gmail.com>
2024-08-17 14:21:09 +0200

    Create POCO Category & Add category property to Pie class

--- Models/Pie.cs
+++ Models/Pie.cs
@@ -12,4 +12,5 @@ public class Pie
     public string? ImageThumbnailUrl { get; set; }
     public bool IsPieOfWeek { get; set; }
     public bool InStock { get; set; }
+    public Category Category { get; set; } = default!;
 }
