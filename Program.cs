f685be8521b91a213ab9eabf729496270ce3c711
Jad Ismail <jadismail665@gmail.com>
2024-08-17 15:30:19 +0200

    Inject PieRepo & CategoryRepo services

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using PieShop.Models.Context;
+using PieShop.Models.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<PieContext>(options => options.UseSqlite("PieShop"));
-    
-var app = builder.Build();
 
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+var app = builder.Build();
 
 app.Run();
