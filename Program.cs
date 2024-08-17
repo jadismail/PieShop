6af836429fa24e93fdff25d1041e9142de06e743
Jad Ismail <jadismail665@gmail.com>
2024-08-17 14:57:08 +0200

    Integrate SQLite db

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PieShop.Models.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<PieContext>(options => options.UseSqlite("PieShop"));
+    
 var app = builder.Build();
 
 
