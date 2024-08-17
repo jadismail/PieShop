f83437334b34653008f4061904310431c27b44c9
Jad Ismail <jadismail665@gmail.com>
2024-08-17 16:57:59 +0200

    Add 3 records to the Sqlite Pie table

--- obj/Debug/net8.0/PieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3696f999e9a0d5e34f27de978e620863a71f593e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23f12e75dd380ef6f78a5287c0395b47cb91a94b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
