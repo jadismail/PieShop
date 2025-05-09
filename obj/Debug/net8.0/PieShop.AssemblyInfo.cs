ddf80b25ef981c78f1d9d6dccc646151fa82e6fa
Jad Ismail <jadismail665@gmail.com>
2025-05-09 23:24:12 +0200

    Ignore the Node_modules/vendor folder

--- obj/Debug/net8.0/PieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b5c0a30633086a5d2ae613b0dd045c23d3c38d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b84d57fc7fb74f8539927ceaa18d788498a611")]
 [assembly: System.Reflection.AssemblyProductAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
